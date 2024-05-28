8f58667e1b63cb9266c85ace5f6ff74fc932bec9
Jenny Haselden <jennyhaselden7@gmail.com>
2024-05-28 15:25:22 -0700

    code compiles

--- HW2/Checker.c
+++ HW2/Checker.c
@@ -1,29 +1,20 @@
-// do this functionality first
 
-/*
-Checker: This program requires two arguments to complete its task. The Checker checks whether or not argTwo (the dividend) is divisible
-by argOne (the divisor) and prints out the result. Both these arguments are positive integers.
-
-The two arguments that the Checker needs to perform its mathematical operation will be supplied to it by the Coordinator; the Coordinator
-is supplied these aforementioned arguments from the command line.
-
-All print statements must indicate the program that is responsible for generating them. To do this, please prefix your print statements
-with the program name i.e. Coordinator or Checker. The example section below depicts these sample outputs.
-*/
-
+#include <libgen.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-
+ 
 int checkDivision(int divisor, int dividend)
 {
     pid_t pid = getpid();
     int returnVal = -1;
-
+ 
     // must print current process id, must match the one returned by the fork() command in step 3
     printf("Checker process [%d]: Starting.\n", pid);
-
+ 
     int remainder = dividend % divisor;
-
+ 
     if (remainder == 0)
     {
         printf("Checker process [%d]: %d *IS* divisible by %d.\n", pid, dividend, divisor);
@@ -34,13 +25,14 @@ int checkDivision(int divisor, int dividend)
         printf("Checker process [%d]: %d *IS NOT* divisible by %d.\n", pid, dividend, divisor);
         returnVal = 0;
     }
-
+ 
     printf("Checker process [%d]: Returning %d.\n", pid, returnVal);
     return returnVal;
 }
-
-
-// int main(){
-//     checkDivision(4, 3);
-//     return 0;
-// }
+ 
+ 
+int main(int argc, char *argv[]){
+    int divisor = atoi(argv[1]);
+    int dividend = atoi(argv[2]);
+    return checkDivision(divisor, dividend);
+}
--- HW2/Coordinator.c
+++ HW2/Coordinator.c
@@ -2,19 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+ 
 int main(int argc, char *argv[])
 {
     pid_t pid;
-    int divisor = argv[1];
+    int divisor = atoi(argv[1]);
     // testing
     printf("divisor: %d", divisor);
-
+ 
     for (int i = 2; i < 5; i++)
     {
         pid = fork();
         printf("Coordinator: forked process with ID %d.", pid);
     }
-
+ 
     return 0;
 }

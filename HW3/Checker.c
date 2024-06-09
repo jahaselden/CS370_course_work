dac822d7c15816731c98877c970b55ca99a662d9
Jenny Haselden <jennyhaselden7@gmail.com>
2024-06-09 13:12:28 -0700

    close with hw3 but not done yet - checkpoint

--- HW3/Checker.c
+++ HW3/Checker.c
@@ -5,35 +5,51 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/resource.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
  
-int checkDivision(int divisor, int dividend, int pid)
+int checkDivision(int divisor, int dividend, int pid, int fd)
 {
- 
+    int sharedMemId = -1;
     int returnVal = -1;
  
     // must print current process id, must match the one returned by the fork() command in step 3
     printf("Checker process [%d]: Starting.\n", pid);
+
+    //retrieve shared memory segment ID from pipe - step 6
+    read(fd, &sharedMemId, sizeof(sharedMemId));
+
+    //get pointer for shared memory
+    int *sharedMemPointer = (int*)shmat(sharedMemId, NULL, 0);
+
+    printf("Checker process [%d]: read %lu bytes containg shm ID %d\n", pid, sizeof(sharedMemId), sharedMemId);
  
     int remainder = dividend % divisor;
  
     if (remainder == 0)
     {
         printf("Checker process [%d]: %d *IS* divisible by %d.\n", pid, dividend, divisor);
+        *sharedMemPointer = 0;
         returnVal = 1;
     }
     else if (remainder != 0)
     {
         printf("Checker process [%d]: %d *IS NOT* divisible by %d.\n", pid, dividend, divisor);
+        *sharedMemPointer = 1;
         returnVal = 0;
     }
  
-    printf("Checker process [%d]: Returning %d.\n", pid, returnVal);
+    printf("Checker process [%d]: wrote result(%d) to shared memory.\n", pid, returnVal);
+
+    //detach from shared mem segment
+    shmdt(sharedMemPointer);
+
     return returnVal;
 }
  
 int main(int argc, char **argv)
 {
-    if (argc != 3)
+    if (argc != 4)
     {
         printf("incorrect number of args");
         return -1;
@@ -42,5 +58,10 @@ int main(int argc, char **argv)
     pid_t pid = getpid();
     int divisor = atoi(argv[1]);
     int dividend = atoi(argv[2]);
-    return checkDivision(divisor, dividend, pid);
+    
+    //retrieve shared memory data
+    int fd = atoi(argv[3]);
+    printf("fd: %d\n", fd);
+    
+    return checkDivision(divisor, dividend, pid, fd);
 }
--- HW3/Coordinator.c
+++ HW3/Coordinator.c
@@ -5,17 +5,27 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/resource.h>
- 
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
 int main(int argc, char *argv[])
 {
     int pid;
     int returnCode = -1;
- 
+
     for (int i = 2; i < 6; i++)
     {
- 
+        // 1. create unique shared memory segments for each Checker instance to store results
+        int sharedMemId = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+
+        // 2. create pipe for each Checker instance that provides it with the ID of the shared
+        //  memory segment created in step 1 - fd of the pipe is passed as an additional
+        //  argument to the checker
+        int fd[2];
+        pipe(fd);
+
         pid = fork();
- 
+
         if (pid == -1)
         {
             printf("fork failed");
@@ -23,19 +33,55 @@ int main(int argc, char *argv[])
         }
         if (pid == 0)
         {
-            execlp("checker", "checker", argv[1], argv[i], NULL); 
+            //create a char array 
+            char buffer[8];
+
+            sprintf(buffer, "%d", fd[0]);
+
+            execlp("checker", "checker", argv[1], argv[i], buffer, NULL); //step 4
         }
         else
         {
             printf("Coordinator: forked process with ID %d.\n", pid);
+
+            // close the read end of the pipe
+            close(fd[0]);
+
+            // write the shared memory id address to pipe - step 5
+            write(fd[1], &sharedMemId, sizeof(sharedMemId));
+
+            // close write end of the pipe
+            close(fd[1]);
+
+            printf("Coordinator: wrote shm ID %d to pipe (%lu bytes)\n", sharedMemId, sizeof(sharedMemId));
             printf("Coordinator: waiting for process [%d].\n", pid);
-            int status;
-            wait(&status);
-            returnCode = WEXITSTATUS(status);
-            printf("Coordinator: child process %d returned %d.\n", pid, returnCode);
+            // int status;
+            // wait(&status);
+            // returnCode = WEXITSTATUS(status);
+            // attach to the shared memory segment
+            int *sharedMemPointer = (int *)shmat(sharedMemId, NULL, 0);
+
+            // read from shared memory segment
+            read(fd[0], &returnCode, sizeof(returnCode));
+
+            // destroy the shared memory segment
+            shmctl(*sharedMemPointer, IPC_RMID, NULL);
+
+            // int status;
+            // wait(&status);
+
+            // change this line to match instructions
+            if (returnCode == 0)
+            {
+                printf("Coordinator: result %d (not divisable) read from shared memory.\n", returnCode);
+            }
+            else if (returnCode == 1)
+            {
+                printf("Coordinator: result %d (divisable) read from shared memory.\n", returnCode);
+            }
         }
     }
- 
+
     printf("Coordinator: exiting.\n");
     return 0;
 }
